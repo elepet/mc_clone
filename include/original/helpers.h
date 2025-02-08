cb1387c7c3bf455579b5cc8f5007fc06ab4412ce
Lev Korsun <z5424890@ad.unsw.edu.au>
2025-02-09 10:27:04 +1100

    bad perspective but rotating cube working. Also sometimes randomly does not render - prob an issue with dynamic allocation

---
+++ include/original/helpers.h
@@ -0,0 +1,1 @@
+const char* rcGetShaderSource(const char* name);
--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
 #define GL_SILENCE_DEPRECATION
-#define GLAD_GL_IMPLEMENTATION
 
+#define GLAD_GL_IMPLEMENTATION
 #include <glad/gl.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #include <GLFW/glfw3.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
-#include "helper.h"
+#include "original/helpers.h"
+#include "original/rml.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -38,8 +42,10 @@ int main() {
 
 	int version = gladLoadGL(glfwGetProcAddress);
 
-	const char* vertexShaderSource = getShaderSource("vertexShader.txt");
-	const char* fragmentShaderSource = getShaderSource("fragmentShader.txt");
+	glEnable(GL_DEPTH_TEST);
+
+	const char* vertexShaderSource = rcGetShaderSource("vertexShader.txt");
+	const char* fragmentShaderSource = rcGetShaderSource("fragmentShader.txt");
 
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -61,8 +67,7 @@ int main() {
 
 	if (!success)
 	{
-		//glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
 	}
 
 	// link shaders
@@ -74,60 +79,163 @@ int main() {
 	// check for linking errors
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success) {
-		//glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n");
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s", infoLog);
 	}
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+//	float vertices[] = {
+//		// positions          // colors           // texture coords
+//		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+//		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+//		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+//		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+//	};
+
 	float vertices[] = {
-	    // positions         // colors
-	     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-	    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-	     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
-	unsigned int indices[] = {
-		0, 1, 3,   // first triangle
-		1, 2, 3    // second triangle
-	};  
+	//unsigned int indices[] = {
+	//	0, 1, 3,   // first triangle
+	//	1, 2, 3    // second triangle
+	//};  
 	
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;//, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	//glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	// texture coord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-
-	glBindVertexArray(0); 
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	// render loop
+	//texture setup
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("../media/container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		printf("ERROR: Failed to load texture\n");
+	}
+	stbi_image_free(data);
+
+	//matrix setup
+	rmlVector* scaleVec = rmlAllocateVector(3);
+	rmlFillVector(0.5, scaleVec);
+	rmlVector* translateVec = rmlAllocateVector(3);
+	translateVec->val[2] = 2.0;
+	rmlVector* rotateVec = rmlAllocateVector(3);
+	rmlFillVector(1.0, rotateVec);
+	rmlNormaliseVector(rotateVec);
+	rmlMatrix* scaleMat = rmlAllocateScalingMatrix(scaleVec);
+	rmlMatrix* translateMat = rmlAllocateTranslationMatrix(translateVec);
+	rmlMatrix* rotateMat = rmlAllocateRotationMatrix(rotateVec, M_PI / 3); 
+	rmlMatrix* transformMat = rmlAllocateDotMatrix(rotateMat,scaleMat); 
+	rmlModifyDotMatrix(translateMat,transformMat,transformMat);
+	int w, h;
+	glfwGetWindowSize(window, &w, &h); 
+	float angleOfView = 90;
+	float near = 0.1;
+	float far = 100;
+	float imageAspectRatio = (float)w / (float)h;
+	//rmlMatrix* projectMat = rmlAllocateProjectionMatrix(angleOfView, imageAspectRatio, near, far);
+	rmlMatrix* projectMat = rmlAllocateProjectionMatrix(h, w, M_PI/2, 1, 40);
+	rmlModifyDotMatrix(projectMat,transformMat,transformMat);	
+
+	//glViewport(-1,-1,w,h);
+
+	//Render loop.
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
 
+		rmlModifyRotationMatrix(rotateVec, (float)glfwGetTime(),  rotateMat);
+		glfwGetWindowSize(window, &w, &h);
+		imageAspectRatio = (float)w / (float)h; 
+		//rmlModifyProjectionMatrix(angleOfView, imageAspectRatio, near, far, projectMat);
+		rmlModifyProjectionMatrix(h, w, M_PI/2, 1, 40, projectMat);
+		rmlModifyDotMatrix(rotateMat, scaleMat, transformMat);
+		rmlModifyDotMatrix(translateMat,transformMat,transformMat);
+		rmlModifyDotMatrix(projectMat,transformMat,transformMat);
+		rmlPrintMatrix(transformMat);
+
+		unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_TRUE, transformMat->val[0]);
+
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		// glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -145,6 +253,16 @@ int main() {
 	}
 
 	glfwTerminate();
+
+	rmlFreeVector(scaleVec);	
+	rmlFreeVector(translateVec);	
+	rmlFreeVector(rotateVec);	
+	rmlFreeMatrix(scaleMat);
+	rmlFreeMatrix(translateMat);
+	rmlFreeMatrix(rotateMat);
+	rmlFreeMatrix(transformMat);
+	rmlFreeMatrix(projectMat);
+
 	return 0;
 }
 
