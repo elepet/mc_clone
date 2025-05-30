df66b6165b5b0bfce6124321127b844c060891b1
Lev Korsun <z5424890@ad.unsw.edu.au>
2025-05-31 08:25:14 +1000

    Switched to cglm, got multiple objects and fly camera working

---
+++ include/standard.h
@@ -0,0 +1,14 @@
+// Single header for standard library headers to avoid duplicates.
+
+#ifndef STANDARD_H
+#define STANDARD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,30 +1,55 @@
-//TODO: model importing, culling, camera, multiple objects, pbr
+// TODO: model importing, culling, camera, multiple objects, pbr.
 
 #define GL_SILENCE_DEPRECATION
-
 #define GLAD_GL_IMPLEMENTATION
 #include <glad/gl.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include <GLFW/glfw3.h>
+#include "cglm/cglm.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdbool.h>
+#include "../include/vertices.h"
+#include "../include/standard.h"
 
+// Settings.
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const unsigned int size = 4;
+double lastTime = 0.0f;
+mat4 model, view, proj, mvp;
+vec3 cameraPos = {0.0f, 0.0f, 3.0f};
+vec3 cameraFront = {0.0f, 0.0f, -1.0f};
+vec3 cameraUp = {0.0f, 1.0f, 0.0f}; 
+vec3 cameraDirection;
+float yaw = -90.0f;
+float pitch = 0.0f;
+bool firstMouse = true;
+const float sensitivity = 0.1f;
+float speed = 10;
 
-#include "original/helpers.h"
-#include "original/rml.h"
+vec3 cubePos[10] = {
+    {0.0f,  0.0f,  0.0f}, 
+    {2.0f,  5.0f, -15.0f}, 
+    {-1.5f, -2.2f, -2.5f},  
+    {-3.8f, -2.0f, -12.3f},  
+    {2.4f, -0.4f, -3.5f},  
+    {-1.7f,  3.0f, -7.5f},  
+    {1.3f, -2.0f, -2.5f},  
+    {1.5f,  2.0f, -2.5f}, 
+    {1.5f,  0.2f, -1.5f}, 
+    {-1.3f,  1.0f, -1.5f}  
+};
 
+double lastX = SCR_WIDTH / 2;
+double lastY = SCR_HEIGHT / 2;
+double deltaX, deltaY;
+
+// Helpers.
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
-void processInput(GLFWwindow *window, float vel[size - 1], double* lastXPos, double* lastYPos, float rot[size - 2]);
-void showDelta(GLFWwindow* window, double* lastTime);
+static void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void processInput(GLFWwindow *window);
+void showDeltaTime(GLFWwindow* window);
+const char* getShaderSource(const char* name);
 
 int main() {
 	glfwInit();
@@ -45,7 +70,7 @@ int main() {
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetCursorPosCallback(window, cursor_position_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	int version = gladLoadGL(glfwGetProcAddress);
@@ -54,18 +79,19 @@ int main() {
 
 	glEnable(GL_CULL_FACE);
 
-	//Default is CCW winding
-	glEnable(GL_CULL_FACE);
+	// Default is CCW winding.
+	glFrontFace(GL_CCW);
+
 	glCullFace(GL_BACK);  
 
-	const char* vertexShaderSource = rcGetShaderSource("vertexShader.txt");
-	const char* fragmentShaderSource = rcGetShaderSource("fragmentShader.txt");
+	const char* vertexShaderSource = getShaderSource("vertex_shader.txt");
+	const char* fragmentShaderSource = getShaderSource("fragment_shader.txt");
 
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 
-	// check for shader compile errors
+	// Check for shader compile errors.
 	int success;
 	char infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -84,13 +110,13 @@ int main() {
 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
 	}
 
-	// link shaders
+	// Link shaders.
 	unsigned int shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 
-	// check for linking errors
+	// Check for linking errors.
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success) {
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
@@ -99,55 +125,6 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	float vertices[] = { //CCW winding
-		// Back face
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Bottom-left
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
-		0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right         
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // bottom-left
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
-						  // Front face
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top-left
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
-						  // Left face
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-left
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
-						  // Right face
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right         
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
-		0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left     
-						 // Bottom face
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
-		0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // top-left
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
-						  // Top face
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right     
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f  // bottom-left        
-	};
-	//unsigned int indices[] = {
-	//	0, 1, 3,   // first triangle
-	//	1, 2, 3    // second triangle
-	//};  
-	
 	unsigned int VBO, VAO;//, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
@@ -161,16 +138,16 @@ int main() {
 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	// position attribute
+	// Position attribute.
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// texture coord attribute
+	// Texture coord attribute.
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	//texture setup
+	// Texture setup.
 	unsigned int texture;
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
@@ -191,78 +168,67 @@ int main() {
 	}
 	stbi_image_free(data);
 
-	//Matrix setup
-	float scaleVec[] = {0.5, 0.5, 0.5};
-	float translateVec1[] = {0.0, 0.0, -2.0};
-	float translateVec2[] = {2.0, 0.0, -2.0};
-	float rotateVecX[] = {1, 0, 0};
-	float rotateVecY[] = {0, 1, 0};
-	float rotateVecZ[] = {0, 0, 1};
-	float scaleMat[size][size];
-	rmlScale(scaleVec, scaleMat);
-	float translateMat[size][size];
-	rmlTranslate(translateVec1, translateMat);
-	float rotateMat[size][size];
-	float rotateMatX[size][size];
-	float rotateMatY[size][size];
-	float rotateMatZ[size][size];
-	float transformMat[size][size];
-	rmlDot(rotateMat,scaleMat,transformMat); 
-	rmlDot(translateMat,transformMat,transformMat);
-	int w, h;
-	glfwGetWindowSize(window, &w, &h); 
-	float projectMat[size][size];
-	float angleOfView = 60; 
-	float n = 0.1; 
-	float f = 100; 
-	rmlProject(angleOfView, w, h, n, f, projectMat);
-	rmlDot(projectMat,transformMat,transformMat);	
-	//glViewport(-1,-1,w,h);
+	// Matrix setup.
+	// Model: object -> world.
+	glm_mat4_identity(model);
+	vec3 translate = {0.0f, 0.0f, -3.0f};
+	glm_translate(model, translate);
+	// View: world -> camera.
+	glm_vec3_add(cameraPos, cameraFront, cameraDirection);
+	glm_lookat(cameraPos, cameraDirection, cameraUp, view);
+	// Projection: camera -> screen.
+	float fov = glm_rad(45.0f);
+	float aspect = (float)SCR_WIDTH / (float)SCR_HEIGHT;
+	float near = 0.1f;
+	float far = 100.0f;
+	glm_perspective(fov, aspect, near, far, proj);
+	// mvp = proj * view * model.
+//	glm_mat4_mul(view, model, mvp);
+//	glm_mat4_mul(proj, mvp, mvp); 
+	// Other.
+	vec3 xAxis = {1.0f, 0.0f, 0.0f};
+	vec3 yAxis = {0.0f, 1.0f, 0.0f};
+	vec3 zAxis = {0.0f, 0.0f, 1.0f};
 
+	// Time setup.
+	lastTime = glfwGetTime();
 
-	//Time setup
-	double lastTime = glfwGetTime();
-
-	//Camera control setup
+	// Camera control setup.
 	float vel[] = {0, 0, 0};
 	float rot[] = {0, 0, 0};
-	double lastXPos, lastYPos;
-	glfwGetCursorPos(window, &lastXPos, &lastYPos);
+	glfwSetCursorPosCallback(window, mouse_callback);
 	
-	//Render loop.
+	// Render loop.
 	while (!glfwWindowShouldClose(window))
 	{
-		processInput(window, vel, &lastXPos, &lastYPos, rot);
+		processInput(window);
 
-		translateVec1[0] += vel[0];
-		translateVec1[1] += vel[1];
-		translateVec1[2] += vel[2];
-		rmlTranslate(translateVec1, translateMat);
-
-		showDelta(window, &lastTime);
+		showDeltaTime(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
 
-		//rmlRotate(rotateVec, (float)glfwGetTime(),  rotateMat);
-		rmlRotate(rotateVecX, rot[0],  rotateMatX);
-		rmlRotate(rotateVecY, rot[1],  rotateMatY);
-		rmlRotate(rotateVecZ, rot[2],  rotateMatZ);
-		glfwGetWindowSize(window, &w, &h);
-		rmlProject(angleOfView, w, h, n, f, projectMat);
-		rmlDot(rotateMatX, scaleMat, transformMat);
-		rmlDot(rotateMatY, transformMat, transformMat);
-		rmlDot(rotateMatZ, transformMat, transformMat);
-		rmlDot(translateMat,transformMat,transformMat);
-		rmlDot(projectMat,transformMat,transformMat);
-
-		unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
-		glUniformMatrix4fv(transformLoc, 1, GL_TRUE, &transformMat[0]);
+		unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, (const float*)view);
+		unsigned int projLoc = glGetUniformLocation(shaderProgram, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, (const float*)proj);
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for(unsigned int i = 0; i < 10; i++) {
+			mat4 model;
+			glm_mat4_identity(model);
+			glm_translate(model, cubePos[i]);
+			glm_rotate(model, (float)glfwGetTime(), yAxis);
+			//glm_mat4_mul(view, model, mvp);
+			//glm_mat4_mul(proj, mvp, mvp);
+
+			unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, (const float*)model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
@@ -285,58 +251,118 @@ int main() {
 	return 0;
 }
 
-void processInput(GLFWwindow *window, float vel[size - 1], double* lastXPos, double* lastYPos, float rot[size - 2])
-{
-	//Close window
+void processInput(GLFWwindow *window) {
+	// Close window.
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 
-	//Movement controls
-	float speed = 0.05;
+	// Movement.
+	double currentTime = glfwGetTime();
+	double deltaTime = (currentTime - lastTime);
 
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) vel[0] = speed;
-	else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) vel[0] = -speed;
-	else vel[0] = 0;
+	const float cameraSpeed = speed *  deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		glm_vec3_muladds(cameraFront, cameraSpeed, cameraPos);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		glm_vec3_muladds(cameraFront, -cameraSpeed, cameraPos);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		vec3 right;
+		glm_vec3_cross(cameraFront, cameraUp, right);
+		glm_vec3_normalize(right);
+		glm_vec3_muladds(right, -cameraSpeed, cameraPos);
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		vec3 right;
+		glm_vec3_cross(cameraFront, cameraUp, right);
+		glm_vec3_normalize(right);
+		glm_vec3_muladds(right, cameraSpeed, cameraPos);
+	}
 
-	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) vel[1] = speed;
-	else if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) vel[1] = -speed;
-	else vel[1] = 0;
+	glm_vec3_add(cameraPos, cameraFront, cameraDirection);
+	glm_lookat(cameraPos, cameraDirection, cameraUp, view);
 
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) vel[2] = speed;
-	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) vel[2] = -speed;
-	else vel[2] = 0;
-
-	//Mouse camera control
-	double xpos, ypos;
-	glfwGetCursorPos(window, &xpos, &ypos);
-	//printf("xpos: %f	ypos: %f\n", xpos, ypos);
-
-	//speed = 0.001;
-	if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) rot[0] += speed;
-	else if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) rot[0] -= speed;
-
-	if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) rot[1] += speed;
-	else if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) rot[1] -= speed;
-
-	if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS) rot[2] += speed;
-	else if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS) rot[2] -= speed;
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
-//Frametime in milliseconds.
-void showDelta(GLFWwindow* window, double* lastTime) {
+// Frametime in milliseconds.
+void showDeltaTime(GLFWwindow* window) {
 	double currentTime = glfwGetTime();
-	double delta = (currentTime - *lastTime) * 1000;
+	double deltaTime = (currentTime - lastTime) * 1000;
 	char str[80];
-	sprintf(str, "Renderer | %.2fms/frame", delta);
+	sprintf(str, "Renderer | %.2fms/frame", deltaTime);
 	glfwSetWindowTitle(window, str);
-	*lastTime = currentTime;
+	lastTime = currentTime;
 }
 
-static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
-	
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	deltaX = xpos - lastX;
+	deltaY = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	deltaX *= sensitivity;
+	deltaY *= sensitivity;
+
+	yaw += deltaX;
+	pitch += deltaY;
+
+	if(pitch > 89.0f)
+		pitch = 89.0f;
+	if(pitch < -89.0f)
+		pitch = -89.0f;
+
+	cameraFront[0] = cos(glm_rad(yaw)) * cos(glm_rad(pitch));
+	cameraFront[1] = sin(glm_rad(pitch));
+	cameraFront[2] = sin(glm_rad(yaw)) * cos(glm_rad(pitch));
+	glm_vec3_normalize(cameraFront);
 }
+
+const char* getShaderSource(const char* name) {
+	FILE* file;
+	char* text;
+	int length;
+	char c;
+	int i = 0; 
+
+	if (strcmp(name, "vertex_shader.txt") == 0) {
+		file = fopen("../src/vertex_shader.txt", "r");
+	} else if (strcmp(name, "fragment_shader.txt") == 0) {
+		file = fopen("../src/fragment_shader.txt", "r");
+	} else {
+		printf("ERROR: Invalid shader source file name.");
+		return NULL;
+	}
+
+	if (file == NULL) {
+		printf("ERROR: Could not open shader source file.");
+		return NULL;
+	}
+
+	fseek(file, 0, SEEK_END);
+	length = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	text = malloc(sizeof(char) * (length + 1));
+
+	while ((c = fgetc(file)) != EOF) {
+		text[i] = c;
+		i++;
+	}
+	text[i] = '\0';
+
+	fclose(file);
+
+	return text;
+}
+
